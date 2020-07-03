414cee0a4ec789bf22e7ce8b7c0699ee2a2eb069
Lucas da Silva <lucas.silva00@outlook.ie>
2020-07-03 18:50:11 +0100

    created profile page

---
+++ web/src/pages/Profile/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+import logoSmall from '../../assets/logo-small.png';
+
+function Profile(){
+  return (
+    <div className="profile-container">
+      <div className="content">
+
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
