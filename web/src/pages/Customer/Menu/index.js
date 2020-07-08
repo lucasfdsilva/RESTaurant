7cf0bc72f1b9d80423db9c4f8a9113e27573e7c1
Lucas da Silva <lucas.silva00@outlook.ie>
2020-07-08 11:59:22 +0100

    menu page complete

--- web/src/pages/Customer/Menu/index.js
+++ web/src/pages/Customer/Menu/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import NavMenu from '../../../components/NavMenu';
+
+import api from '../../../services/api';
 
 import './styles.css';
 
 function Login(){
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    async function loadMenuItems(){
+    try {
+      
+      const response = await api.get(`/menu`);
+      setMenuItems(response.data);
+
+    } catch (error) {
+      alert(`Couldn't Load Menu Items. Please try again. Error: ${error}.`);
+    }
+  }
+  loadMenuItems();
+  }, [])
 
   const history = useHistory();
 
@@ -14,6 +31,24 @@ function Login(){
 
       <div className="content">
         <h1>Menu Items</h1>  
+
+        <ul>
+          {menuItems.map(item => (
+            <li key={item.id}>
+              <strong>Menu Item ID:</strong>
+              <p>{item.id}</p>
+
+              <strong>Name:</strong>
+              <p>{item.name}</p>
+
+              <strong>Price:</strong>
+              <p>€{item.price}</p>
+
+              <strong>Description:</strong>
+              <p>{item.description}</p>
+            </li>
+          ))}
+        </ul>
       </div>
 
     </div>
