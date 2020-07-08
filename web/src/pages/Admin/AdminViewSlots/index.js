ed08797c107c13491756f25dfb4a88eef9a54e15
Lucas da Silva <lucas.silva00@outlook.ie>
2020-07-08 18:43:09 +0100

    refactored admin panel styling to fit components correctly

--- web/src/pages/Admin/AdminViewSlots/index.js
+++ web/src/pages/Admin/AdminViewSlots/index.js
@@ -32,61 +32,29 @@ function AdminViewSlots(){
 
   return (
     <div className="admin-slots-container">
-      <header className="top-navigation">
-      <Link className="header-link" to="/admin">Admin Panel</Link>
-      <div class="dropdown">
-          <Link className="header-link" to="/">Users</Link>
-          <div class="dropdown-content">
-            <Link className="header-link" to="/">View User</Link>
-            <Link className="header-link" to="/">Create User</Link>
-          </div>
-        </div>
-
-        <div class="dropdown">
-          <Link className="header-link" to="/">Time Slots</Link>
-          <div class="dropdown-content">
-            <Link className="header-link" to="/admin/slots">View Timeslots</Link>
-            <Link className="header-link" to="/">Create Timeslot</Link>
-          </div>
-        </div>
-
-        <div class="dropdown">
-          <Link className="header-link" to="/">Bookings</Link>
-          <div class="dropdown-content">
-            <Link className="header-link" to="/">View Bookings</Link>
-          </div>
-        </div>
-
-        <Link className="header-link" to="/"> Logout </Link>
-      </header>
-
       <h1>Registered Slots</h1>
 
-      <div className="slots">
-        <ul>
-          {slots.map(slot => (
+      <ul>
+        {slots.map(slot => (
+          <li key={slot.id}>
+            <strong>Slot ID:</strong>
+            <p>{slot.id}</p>
 
-            <li key={slot.id}>
-              <strong>Slot ID:</strong>
-              <p>{slot.id}</p>
+            <strong>Start Time:</strong>
+            <p>{slot.start_time}</p>
 
-              <strong>Start Time:</strong>
-              <p>{slot.start_time}</p>
+            <strong>Duration:</strong>
+            <p>{slot.duration} minutes</p>
 
-              <strong>Duration:</strong>
-              <p>{slot.duration} minutes</p>
+            <strong>Max Capacity:</strong>
+            <p>{slot.max_capacity} people</p>
 
-              <strong>Max Capacity:</strong>
-              <p>{slot.max_capacity} people</p>
+            <strong>Weekdays:</strong>
+            <p>{slot.week_days}</p>
 
-              <strong>Weekdays:</strong>
-              <p>{slot.week_days}</p>
-
-            </li>
-          ))}
-        </ul>
-      </div>
-    
+          </li>
+        ))}
+      </ul>
     </div>
 
     
