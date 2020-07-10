be59531d0a6bb261f78a4f348a9b71a24d1422f2
Lucas da Silva <lucas.silva00@outlook.ie>
2020-07-10 13:00:41 +0100

    pm2 app config file added to web

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routes = require('./routes');
 const cors = require('cors')
+const routes = require('./routes');
 
 //Configuring Express Server
 const app = express();
