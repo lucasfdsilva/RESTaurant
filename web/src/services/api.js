939a6384b60434b5282cf929025e9fab1b586d10
Lucas da Silva <lucas.silva00@outlook.ie>
2020-07-10 15:57:55 +0100

    updated axios baseURL to use LB dns

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://restaurant-classiclb-2078999781.eu-west-1.elb.amazonaws.com:3000'
 });
 
 export default api;
