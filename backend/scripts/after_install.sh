c92fc26580140fc03b9d2deed6bbf93840ae523a
Lucas da Silva <lucas.silva00@outlook.ie>
2020-07-10 17:09:53 +0100

    test

--- backend/scripts/after_install.sh
+++ backend/scripts/after_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Installing NPM Dependencies"
-sudo npm install /home/ec2-user/restaurant-management/backend/
-sudo npm install /home/ec2-user/restaurant-management/web/
-cd /home/ec2-user/restaurant-management/web/
-sudo npm run build
+sudo npm install --prefix /home/ec2-user/restaurant-management/backend/
+sudo npm install --prefix /home/ec2-user/restaurant-management/web/
+sudo npm run build --prefix /home/ec2-user/restaurant-management/web/
