619f1278ae22490a8ce2f83b641266bc874d6dc7
Lucas da Silva <lucas.silva00@outlook.ie>
2020-07-16 17:17:03 +0100

    updated website domain

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://restaurant-classiclb-2078999781.eu-west-1.elb.amazonaws.com:3000'
+  baseURL: 'https://asystec-restaurant.co.uk:3000'
   //baseURL: 'http://localhost:3000'
 });
 
